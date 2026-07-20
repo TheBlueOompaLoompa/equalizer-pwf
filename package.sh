bf02e1e954c9a42fad55c02ccf7897a717f31594
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2026-07-20 02:56:03 -0500

    Recurse submodules

--- package.sh
+++ package.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+mkdir -p flatpak-build
 flatpak-builder --repo=flatpak-build/repo flatpak-build org.bloompa.EqualizerPWF.yml --user --install-deps-from=flathub --force-clean
 flatpak build-bundle flatpak-build/repo flatpak-build/org.bloompa.EqualizerPWF.flatpak org.bloompa.EqualizerPWF
 
